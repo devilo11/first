36805893839ac2b6d5221adce21cd917b67cef0c
devilo <gou43033039@163.com>
2024-12-19 12:30:21 +0800

    Add files via upload

---
+++ Project1/arr.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+//void printstar(int n) /*n可以全局使用没限制*/
+//{
+//	printf("一个等差数列\n");
+//	int a, x;/*a和x都是局部变量只能在这个大括号内用*/
+//	for (a = 1; a <= n; a++)
+//	{
+//		for (x = 1; x <= a; x++)
+//			printf("*");
+//		printf("\n");
+//
+//	}
+//}
+
+
+int main(void)
+{
+	int f, g;
+
+	 
+	return 0;
+}
+/*这傻逼代码多个void给我整出来了个重复命名*/
+ fun1(int h)  
+{
+	if (h != 4)
+	{
+		
+		fun1(h-1);
+		printf("%d\n", h);
+	}
+	//int fun(void)
+//{
+//	printf("zzz\n" );
+//	return 3 * 3;
+//
+//}
+	/*int c = fun();
+	printf("%d %d\n", c, fun());*/
+	/*int n;
+	printf("第一\n");
+	scanf_s("%d", &n);
+	printstar(n);
+	return 0;*/
+
+}
+
